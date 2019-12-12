6ad570970af9d4796ceceec0d6edbed1fcf4a69d
Hank Wang <drapho@gmail.com>
2019-12-13 02:24:31 +0900

    update python solution: scrabble_score

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,15 +1,15 @@
 def score(word):
     scores = 0
     for character in word:
-        for chars, _score in {
-            ("a", "e", "i", "o", "u", "l", "n", "r", "s", "t"): 1,
-            ("d", "g"): 2,
-            ("b", "c", "m", "p"): 3,
-            ("f", "h", "v", "w", "y"): 4,
-            ("k",): 5,
-            ("j", "x"): 8,
-            ("q", "z"): 10
+        for chars, score in {
+            "aeioulnrst": 1,
+            "dg": 2,
+            "bcmp": 3,
+            "fhvwy": 4,
+            "k": 5,
+            "jx": 8,
+            "qz": 10
         }.items():
             if character.lower() in chars:
-                scores += _score
+                scores += score
     return scores
