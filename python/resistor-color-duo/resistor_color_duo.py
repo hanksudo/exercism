efa22bb0d365466d1f83df20952ed03e20a7b994
Hank Wang <drapho@gmail.com>
2020-03-10 23:01:44 +0900

    [python] resistor_color_duo

---
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,3 @@
+def value(colors):
+    _colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+    return _colors.index(colors[0]) * 10 + _colors.index(colors[1])
