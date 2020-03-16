9036b563ffe030330b817ba930fcddfebfc317c7
Hank Wang <drapho@gmail.com>
2020-03-16 13:52:24 +0900

    [python] isbn-verifier

---
+++ python/isbn-verifier/isbn_verifier.py
@@ -0,0 +1,16 @@
+def is_valid(isbn):
+    isbn = isbn.replace("-", "")
+    if len(isbn) != 10:
+        return False
+
+    sum_of_values = 0
+    for idx, c in enumerate(isbn.replace("-", "")):
+        try:
+            sum_of_values += (10 - idx) * int(c)
+        except ValueError:
+            if idx == 9 and c == 'X':
+                sum_of_values += 10
+            else:
+                return False
+
+    return sum_of_values % 11 == 0
