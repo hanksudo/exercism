2e0c34583e467bcb3456539d950b929327d6ff79
Hank Wang <drapho@gmail.com>
2020-03-16 13:35:38 +0900

    [rust] set ONE_BILLION by constants

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
 use chrono::{DateTime, Utc, Duration};
 
+const ONE_BILLION: i64 = 1_000_000_000;
+
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(10_i64.pow(9))
+    start + Duration::seconds(ONE_BILLION)
 }
