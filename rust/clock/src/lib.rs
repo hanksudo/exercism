9167cf2175fd9ae1c90df43b04c30e4a91290dae
Hank Wang <drapho@gmail.com>
2020-03-20 01:43:01 +0900

    [rust] clock

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,25 @@
+use std::fmt;
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub struct Clock {
+    minutes: i32
+}
+
+const MINUTES_OF_DAY: i32 = 60 * 24;
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        Clock { minutes: (hours * 60 + minutes).rem_euclid(MINUTES_OF_DAY) }
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Self::new(0, self.minutes + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", (self.minutes / 60) % 24, self.minutes % 60)
+    }
+}
