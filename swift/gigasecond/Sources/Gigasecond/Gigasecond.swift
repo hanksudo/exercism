ea8cc74d59c48659207245780cdb141d5fb066da
Hank Wang <drapho@gmail.com>
2020-06-23 23:36:50 +0900

    [swift] gigasecond

---
+++ swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+//Solution goes in Sources
+struct Gigasecond {
+    let description: String
+    let one_billion = 1_000_000_000.0
+    static let formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "en_US_POSIX")
+        f.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        f.timeZone = TimeZone(secondsFromGMT: 0)
+        return f
+    }()
+
+    init?(from: String) {
+        if let date = Gigasecond.formatter.date(from: from) {
+            let hence = Date(timeInterval: one_billion, since: date)
+            description = Gigasecond.formatter.string(from: hence)
+        } else {
+            return nil
+        }
+    }
+}
