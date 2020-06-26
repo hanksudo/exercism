703c9bc973c5ce82df3e54fccc45bf7741de071b
Hank Wang <drapho@gmail.com>
2020-06-27 01:05:56 +0900

    [swift] sum of multiples

---
+++ swift/sum-of-multiples/Sources/SumOfMultiples/SumOfMultiples.swift
@@ -0,0 +1,18 @@
+//Solution goes in Sources
+struct SumOfMultiples {
+    static func toLimit(_ limit: Int, inMultiples: [Int]) -> Int {
+        if limit == 0 {
+            return 0
+        }
+        var sum: Int = 0
+        for i in 1..<limit {
+            for j in inMultiples {
+                if j > 0 && i % j == 0 {
+                    sum += i
+                    break
+                }
+            }
+        }
+        return sum
+    }
+}
