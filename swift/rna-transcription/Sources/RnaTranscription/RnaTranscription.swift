97c51401002373e1d4de31cb325884ff8b2a5c81
Hank Wang <drapho@gmail.com>
2020-06-27 01:38:09 +0900

    [swift] rna transcription

---
+++ swift/rna-transcription/Sources/RnaTranscription/RnaTranscription.swift
@@ -0,0 +1,26 @@
+//Solution goes in Sources
+struct RnaTranscription {
+    enum TranscriptionError: Error {
+        case invalidNucleotide(_ message: String)
+    }
+}
+struct Nucleotide {
+    var dna_strand: String = ""
+    var transcribed: [Character: Character] = [
+        "C": "G",
+        "G": "C",
+        "T": "A",
+        "A": "U",
+    ]
+    init(_ strand: String) {
+        dna_strand = strand
+    }
+    func complementOfDNA() throws -> String {
+        return try dna_strand.map {
+            guard let complement = transcribed[$0] else {
+                throw RnaTranscription.TranscriptionError.invalidNucleotide("\($0) is not a valid Nucleotide")
+            }
+            return String(complement)
+        }.joined()
+    }
+}
