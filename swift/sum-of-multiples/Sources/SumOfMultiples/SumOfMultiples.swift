72e68180505b6b33c14330aad117e7baa8c35595
Hank Wang <drapho@gmail.com>
2020-07-02 13:14:29 +0900

    [swift] improve sum of multiples

--- swift/sum-of-multiples/Sources/SumOfMultiples/SumOfMultiples.swift
+++ swift/sum-of-multiples/Sources/SumOfMultiples/SumOfMultiples.swift
@@ -1,7 +1,7 @@
 //Solution goes in Sources
 struct SumOfMultiples {
     static func toLimit(_ limit: Int, inMultiples: [Int]) -> Int {
-        guard limit != 0 else { return 0 }
+        guard limit > 0 else { return 0 }
         return (1..<limit).filter { num in
             inMultiples.contains { num.isMultiple(of: $0) }
         }.reduce(0, +)
